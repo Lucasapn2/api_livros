745d18968bbd5987621174fcc3d7cf3d82786afc
Lucas Ferreira <129186705+Lucasapn2@users.noreply.github.com>
2024-08-30 10:46:34 -0300

    Update database.py

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 import os
 
-
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Remova o `connect_args` porque não é necessário para bancos de dados como PostgreSQL ou MySQL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
