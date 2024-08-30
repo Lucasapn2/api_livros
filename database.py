d62ed66905a2e1449e548bcf7ed3020af27b2898
Lucas Ferreira <129186705+Lucasapn2@users.noreply.github.com>
2024-08-30 13:04:20 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
-import os
 
-# Substitua pela URL apropriada
-SQLALCHEMY_DATABASE_URL = "postgresql://bdapp_n8co_user:0MwMppnz7IwL7QtMUD5mj4ov01oKyrDV@dpg-cr8uooi3esus73bdjiig-a.oregon-postgres.render.com/bdapp_n8co"
+# Obtenha a URL do banco de dados da variável de ambiente
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
